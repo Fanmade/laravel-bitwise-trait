990020a360bf35224b0dcb4281ae13468b83363c
Ben <ben@fanmade.de>
2017-05-15 16:58:24 +0200

    Create BitwiseFlagTrait.php

---
+++ BitwiseFlagTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Traits;
+
+/**
+ * Class BitwiseFlagTrait
+ * @package App\Traits
+ */
+trait BitwiseFlagTrait
+{
+
+    /**
+     *
+     */
+    protected static function bootBitwiseFlagTrait()
+    {
+        //
+    }
+
+    /**
+     * @param $name
+     * @param $flag
+     * @return bool
+     */
+    protected function isFlagSet($name, $flag)
+    {
+        return (($this->$name & $flag) == $flag);
+    }
+
+    /**
+     * @param string $name
+     * @param int $flag
+     * @param $value
+     * @return bool
+     */
+    protected function setFlag($name, $flag, $value)
+    {
+        if ($value) {
+            $this->$name |= $flag;
+        } else {
+            $this->$name &= ~$flag;
+        }
+    }
+}
